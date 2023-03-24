bc423d00082529560ccc1c24b4ad52c5a784ed36
IMAMSGIT <imamhosain95@gmail.com>
2023-03-24 19:51:45 +0600

    Conditional rendering with truthy,falsy

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -3,36 +3,27 @@ class Counter extends Component {
   // state object to pass data
   stateExample = {
     countNow: 1,
-    tags: ["tag1", "tag2", "tag3"],
+    tags: [],
   };
   // must render method
+
+  conditionalRender() {
+    if (this.stateExample.tags.length === 0) return <p>There are no tags!!</p>;
+    return (
+      <ul>
+        {this.stateExample.tags.map((oneTag) => (
+          <li key={oneTag}>{oneTag}</li>
+        ))}
+      </ul>
+    );
+  }
   render() {
     return (
       <div>
-        <span className={this.showColorOrWarning()}>{this.showCount()}</span>
-        <button className="btn btn-secondary btn-sm">Increment</button>
-        <ul>
-          {this.stateExample.tags.map((oneTag) => (
-            <li key={oneTag}>{oneTag}</li>
-          ))}
-        </ul>
+        {this.stateExample.tags.length === 0 && "Please add one element"}
+        {this.conditionalRender()}
       </div>
     );
-  }
-
-  // To show zero or count
-  showCount() {
-    // Object destructuring
-    // set alias for countNow as count
-    const { countNow: count } = this.stateExample;
-    return count === 0 ? "Zero" : count;
-  }
-
-  showColorOrWarning() {
-    let classNames = "badge bg-";
-    classNames +=
-      this.stateExample.countNow === 0 ? "warning m-2" : "primary m-2";
-    return classNames;
   }
 }
 
