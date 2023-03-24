75368a0fe65a7ff0f0d95887b8538ac3e4afac85
IMAMSGIT <imamhosain95@gmail.com>
2023-03-25 00:17:30 +0600

    COnditional rendering- badge warning or primary

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,28 +2,27 @@ import React, { Component } from "react";
 class Counter extends Component {
   // state object to pass data
   stateExample = {
-    countNow: 1,
-    tags: [],
+    countNow: 44,
   };
   // must render method
 
-  conditionalRender() {
-    if (this.stateExample.tags.length === 0) return <p>There are no tags!!</p>;
-    return (
-      <ul>
-        {this.stateExample.tags.map((oneTag) => (
-          <li key={oneTag}>{oneTag}</li>
-        ))}
-      </ul>
-    );
-  }
   render() {
     return (
       <div>
-        {this.stateExample.tags.length === 0 && "Please add one element"}
-        {this.conditionalRender()}
+        <span className={this.changeBadge()}>{this.showCount()}</span>
+        <button className="btn btn-secondary btn-sm">Increment</button>
       </div>
     );
+  }
+  showCount() {
+    return this.stateExample.countNow === 0
+      ? "Zero"
+      : this.stateExample.countNow;
+  }
+  changeBadge() {
+    let classes = "badge bg-";
+    classes += this.stateExample.countNow === 0 ? "warning m-2" : "primary m-2";
+    return classes;
   }
 }
 
