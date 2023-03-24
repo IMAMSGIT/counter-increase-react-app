34d3731db5333db86579ec73a88e6e445af5c8cd
IMAMSGIT <imamhosain95@gmail.com>
2023-03-24 19:40:07 +0600

    Upto rendering lists

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,74 +1,38 @@
 import React, { Component } from "react";
 class Counter extends Component {
-  // special state property/object render method needs
-  state = {
-    count: 0,
-    // rendering list
+  // state object to pass data
+  stateExample = {
+    countNow: 1,
     tags: ["tag1", "tag2", "tag3"],
-    // imageUrl: "https://picsum.photos/200",
   };
-
-  // render method
+  // must render method
   render() {
     return (
       <div>
-        {/* /* image added */}
-        {/* <img src={this.state.imageUrl} alt="" /> */}
-        <span className={this.getBadgeClasses()}>{this.formatCount()} </span>
-
-        {/* Handling events */}
-        {/* and when arrow function has parameters */}
-        <button
-          onClick={() => this.handleIncrement({ id: 1 })}
-          className="btn btn-secondary btn-sm"
-        >
-          Increment
-        </button>
-
-        {/* Rendering list */}
+        <span className={this.showColorOrWarning()}>{this.showCount()}</span>
+        <button className="btn btn-secondary btn-sm">Increment</button>
         <ul>
-          {this.state.tags.map((tag) => (
-            <li key={tag}>{tag}</li>
+          {this.stateExample.tags.map((oneTag) => (
+            <li key={oneTag}>{oneTag}</li>
           ))}
         </ul>
-        {this.renderTags()}
-
-        {/* Conditional rendering */}
-        {this.state.tags.length === 0 && "Please create a new tag"}
       </div>
     );
   }
 
-  // Event handler function for button clicked
-  handleIncrement = (product) => {
-    // check to all are okay
-    // console.log("Increment clicked", this);
-    console.log(product);
-    this.setState({ count: this.state.count + 1 });
-  };
-
-  getBadgeClasses() {
-    let classes = "badge m-1 bg-";
-    classes += this.state.count === 0 ? "warning" : "primary";
-    return classes;
-  }
-
-  // conditional rendering
-  renderTags() {
-    if (this.state.tags.length === 0) return <p>There are no tags</p>;
-    return (
-      <ul>
-        {this.state.tags.map((tag) => (
-          <li key={tag}>{tag}</li>
-        ))}
-      </ul>
-    );
-  }
-
-  // holding state variables value
-  formatCount() {
-    const { count } = this.state;
+  // To show zero or count
+  showCount() {
+    // Object destructuring
+    // set alias for countNow as count
+    const { countNow: count } = this.stateExample;
     return count === 0 ? "Zero" : count;
+  }
+
+  showColorOrWarning() {
+    let classNames = "badge bg-";
+    classNames +=
+      this.stateExample.countNow === 0 ? "warning m-2" : "primary m-2";
+    return classNames;
   }
 }
 
