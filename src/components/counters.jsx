1dc60c5abd9aec286e4f8c21a4b64ca3cc58ad13
IMAMSGIT <imamhosain95@gmail.com>
2023-03-30 01:47:20 +0600

    sateless functional component,argument destructure

--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -2,20 +2,18 @@ import React, { Component } from "react";
 import Counter from "./counter";
 class Counters extends Component {
   render() {
+    const { onReset, countersPass, onDelete, onIncrement } = this.props;
     return (
       <div>
-        <button
-          onClick={this.props.onReset}
-          className="btn btn-primary btn-sm m-2"
-        >
+        <button onClick={onReset} className="btn btn-primary btn-sm m-2">
           Reset
         </button>
 
-        {this.props.countersPass.map((cPass) => (
+        {countersPass.map((cPass) => (
           <Counter
             key={cPass.id}
-            onIncrement={this.props.onIncrement}
-            onDelete={this.props.onDelete}
+            onIncrement={onIncrement}
+            onDelete={onDelete}
             // counterTogether captures all counterPass
             counterTogether={cPass}
             // valueC={cPass.value}
--- src/components/navBar.jsx
+++ src/components/navBar.jsx
@@ -13,5 +13,20 @@ class NavBar extends Component {
     );
   }
 }
-
 export default NavBar;
+
+// stateless functional component- same result
+// const NavBar1 = (props) => {
+//   return (
+//     <nav className="navbar navbar-light bg-light">
+//       <a className="navbar-brand">
+//         Navigation Bar
+//         <span className="badge rounded-pill text-bg-secondary m-1">
+//           {props.totalCounter}
+//         </span>
+//       </a>
+//     </nav>
+//   );
+// };
+
+//export default NavBar1;
